4d8f77151dc7a9db5b7c6235d0dc3ca35a342243
Iremel Iskuzhin <9588504@gmail.com>
2022-10-23 15:47:00 +0500

    hw solutions

---
+++ hw_game.py
@@ -0,0 +1,57 @@
+"""Игра угадай число
+Компьютер сам загадывает и сам угадывает число
+"""
+
+import numpy as np
+
+
+def random_predict(number: int = 1) -> int:
+    """Рандомно угадываем число
+
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
+    count = 0
+    min, max = 1, 100
+    predict_number = np.random.randint(min, max + 1)  # предполагаемое число
+    while True:
+        count += 1
+        if number == predict_number:
+            break  # выход из цикла если угадали
+        elif number > predict_number:
+            min =  predict_number + 1
+            predict_number = (max + min) // 2
+        elif number < predict_number:
+            max = predict_number - 1
+            predict_number = (max + min) // 2
+
+    return count
+
+
+def score_game(random_predict) -> int:
+    """За какое количство попыток в среднем за 1000 подходов угадывает наш алгоритм
+
+    Args:
+        random_predict ([type]): функция угадывания
+
+    Returns:
+        int: среднее количество попыток
+    """
+    count_ls = []
+    #np.random.seed(1)  # фиксируем сид для воспроизводимости
+    random_array = np.random.randint(1, 101, size=(1000))  # загадали список чисел
+
+    for number in random_array:
+        count_ls.append(random_predict(number))
+
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за: {score} попыток!")
+    return score
+
+
+if __name__ == "__main__":
+    # RUN
+    score_game(random_predict)
